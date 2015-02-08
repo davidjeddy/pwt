ceda8faeec9a78471e20207a3154e2557f739ec5
David J Eddy <me@davidjeddy.com>
2015-02-08 18:10:29 -0500

    Basic functionality complete for base set of utilities and tools.

--- pwt.php
+++ pwt.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * `PHP Workflow Tools` is a class that invokes a set of PHP QA tools using a single CLI command
+ *
+ * @package davidjeddy
+ * @subpackage  PWT
+ * @author  David J Eddy <me@davidjeddy.com>
+ * @version  0.2
+ */
 require_once('./vendor/autoload.php');
 
 /**
@@ -31,9 +39,7 @@ class pwt
         $this->lopts  = [
             "exclude::",
             "output::",
-            "progress::",
             "source::",
-            "verbose::",
         ];
 
         if ($this->mapCLItoProperties()) {
@@ -44,18 +50,29 @@ class pwt
     }
 
     /**
-     * Map our CLI options to the CLi options of each tool
+     * Map our CLI options to the CLI options of each tool
+     * 
      * @return boolean
      */
     private function mapCLItoProperties()
     {
+        // get provided CLI options
         $this->args = getopt(null, $this->lopts);
 
-        if (!isset($this->args["output"])) {
-            $this->args["output"] = "./_output/";
+
+        // set the default output dir
+        if (isset($this->args["output"])) {
+        	if (file_exists($this->args["output"])) {
+        		system("mkdir ".$this->args['source']."_archives/");
+        		system ("tar -zcf ".$this->args['source']."_archives/".time().".tar.gz ".$this->args['output']." && rm -rf ".$this->args['output']);
+        	}
+            
+            system('mkdir '.$this->args["output"]);
+
+            return true;
         }
 
-        return true;
+        return false;
     }
 
     /**
@@ -64,27 +81,31 @@ class pwt
      */
     private function doIt()
     {
-        system('mkdir '.$this->args['output']."pdepend");
-
-        // sebastian utilities
-        // --progress=true --exclude="./vendor" --source="./"
-        // cs-fixer uses  config file instead of CLI options. Make the file now
-        /*
-        system('php ./vendor/sebastian/phpcpd/phpcpd --exclude="'.$this->args['exclude'].'" '.$this->args['source'].' \
-            --progress');
-        system('php ./vendor/sebastian/phpdcd/phpdcd --exclude="'.$this->args['exclude'].'" '.$this->args['source'].' \
-            --recursive');
-        system("./vendor/pdepend/pdepend/src/bin/pdepend \
+        system("mkdir ".$this->args['output']."pdepend/ && ./vendor/pdepend/pdepend/src/bin/pdepend \
             --summary-xml=".$this->args['output']."pdepend/summary.xml \
             --jdepend-chart=".$this->args['output']."pdepend/jdepend.svg \
             --overview-pyramid=".$this->args['output']."pdepend/pyramid.svg \
             --ignore=".$this->args['exclude']." \
             ".$this->args['source']);
-        system('./vendor/fabpot/php-cs-fixer/php-cs-fixer fix '.$this->args['source']);
-        */
+        system("mkdir ".$this->args['output']."phpdocs/ && php ./vendor/phpdocumentor/phpdocumentor/bin/phpdoc \
+        	--directory=".$this->args['source']." \
+        	--target=./".$this->args['output']."phpdocs/ \
+        	--ignore=".$this->args['exclude']."/ \
+        	--log=".$this->args['output']."/phpdocs/logs \
+        	--progressbar");
+        system("mkdir ".$this->args['output']."phploc/ && php ./vendor/phploc/phploc/phploc \
+        	--log-csv ".$this->args['output']."/phploc/phploc.csv \
+        	--git-repository ".$this->args['source']." \
+        	src --progress");
+        system("mkdir ".$this->args['output']."phpcpd/ && php ./vendor/sebastian/phpcpd/phpcpd \
+        	--exclude=".$this->args['exclude']." ".$this->args['source']." \
+            --progress > ".$this->args['output']."/phpcpd/phpcpd.log");
+        system("mkdir ".$this->args['output']."phpdcd/ && php ./vendor/sebastian/phpdcd/phpdcd \
+        	--exclude=".$this->args['exclude']." ".$this->args['source']." \
+            --recursive > ".$this->args['output']."/phpdcd/phpdcd.log");
 
         return true;
     }
 }
 
-$pwt = new PWT();
+new PWT();
