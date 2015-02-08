269638836b3360b339b2300efe1f666d9958da54
David J Eddy <me@davidjeddy.com>
2015-02-08 11:28:02 -0500

    Init functionality of cs-fixer working.

--- pwt.php
+++ pwt.php
@@ -1,88 +1,90 @@
 <?php
+require_once('./vendor/autoload.php');
+
 /**
  * Welcome to PWT...
  *
  * @author  David Eddy <me@davidjeddy.com>
  */
-class PWT
+class pwt
 {
-	/**
-	 * Container for CLI options
-	 * 
-	 * @var [type]
-	 */
-	private $args;
+    /**
+     * Container for CLI options
+     *
+     * @var [type]
+     */
+    private $args;
 
-	/**
-	 * PHP long options for getopt()
-	 * @var [type]
-	 */
-	private $lopts;
+    /**
+     * PHP long options for getopt()
+     * @var [type]
+     */
+    private $lopts;
 
-	/**
-	 * 
-	 */
-	public function __construct()
-	{
-		global $argv;
+    /**
+     *
+     */
+    public function __construct()
+    {
+        global $argv;
 
-		$this->lopts  = [
-			"exclude::",
-			"output::",
-			"progress::",
-			"source::",
-			"verbose::",
-		];
+        $this->lopts  = [
+            "exclude::",
+            "output::",
+            "progress::",
+            "source::",
+            "verbose::",
+        ];
 
-		if ($this->mapCLItoProperties()) {
-			return $this->doIt();
-		}
+        if ($this->mapCLItoProperties()) {
+            return $this->doIt();
+        }
 
-		return false;
-	}
+        return false;
+    }
 
-	/**
-	 * Map our CLI options to the CLi options of each tool
-	 * @return boolean
-	 */
-	private function mapCLItoProperties()
-	{
+    /**
+     * Map our CLI options to the CLi options of each tool
+     * @return boolean
+     */
+    private function mapCLItoProperties()
+    {
+        $this->args = getopt(null, $this->lopts);
 
-		$this->args = getopt(null, $this->lopts);
+        if (!isset($this->args["output"])) {
+            $this->args["output"] = "./_output/";
+        }
 
-		if (!isset($this->args["output"])) {
-			$this->args["output"] = "./_output/";
-		}
+        return true;
+    }
 
-		return true;
-	}
+    /**
+     * Execute CLI commands
+     * @return boolean
+     */
+    private function doIt()
+    {
+        system('mkdir '.$this->args['output']."pdepend");
 
-	/**
-	 * Execute CLI commands
-	 * @return boolean
-	 */
-	private function doIt() {
+        // sebastian utilities
+        // --progress=true --exclude="./vendor" --source="./"
+        // cs-fixer uses  config file instead of CLI options. Make the file now
+        /*
+        system('php ./vendor/sebastian/phpcpd/phpcpd --exclude="'.$this->args['exclude'].'" '.$this->args['source'].' \
+            --progress');
+        system('php ./vendor/sebastian/phpdcd/phpdcd --exclude="'.$this->args['exclude'].'" '.$this->args['source'].' \
+            --recursive');
+        system("./vendor/pdepend/pdepend/src/bin/pdepend \
+            --summary-xml=".$this->args['output']."pdepend/summary.xml \
+            --jdepend-chart=".$this->args['output']."pdepend/jdepend.svg \
+            --overview-pyramid=".$this->args['output']."pdepend/pyramid.svg \
+            --ignore=".$this->args['exclude']." \
+            ".$this->args['source']);
+        system('./vendor/fabpot/php-cs-fixer/php-cs-fixer fix '.$this->args['source']);
+        */
 
-		system('mkdir '.$this->args['output']."pdepend");
-
-		// sebastian utilities
-		// --progress=true --exclude="./vendor" --source="./"
-		system('./vendor/fabpot/php-cs-fixer/php-cs-fixer fix '.$this->args['source'].' exclude="'.$this->args['exclude'].'"');
-		/*
-		system('php ./vendor/sebastian/phpcpd/phpcpd --exclude="'.$this->args['exclude'].'" '.$this->args['source'].' \
-			--progress');
-		system('php ./vendor/sebastian/phpdcd/phpdcd --exclude="'.$this->args['exclude'].'" '.$this->args['source'].' \
-			--recursive');
-		system("./vendor/pdepend/pdepend/src/bin/pdepend \
-			--summary-xml=".$this->args['output']."pdepend/summary.xml \
-			--jdepend-chart=".$this->args['output']."pdepend/jdepend.svg \
-			--overview-pyramid=".$this->args['output']."pdepend/pyramid.svg \
-			--ignore=".$this->args['exclude']." \
-			".$this->args['source']);
-		*/
-
-		return true;
-	}
+        return true;
+    }
 }
 
 $pwt = new PWT();
